1095275107e019d2bed197d516abda37797107d9
RomanBachaloSigmaSoftware <roman.bachalo@sigma.software>
2021-02-08 22:23:31 +0200

    PermissionCreate update

--- launcher-csharp/eSignature/Examples/CreatePermissionProfile.cs
+++ launcher-csharp/eSignature/Examples/CreatePermissionProfile.cs
@@ -1,9 +1,9 @@
-﻿using DocuSign.eSign.Api;
+﻿using DocuSign.CodeExamples.Models;
+using DocuSign.eSign.Api;
 using DocuSign.eSign.Client;
 using DocuSign.eSign.Model;
 
-
-namespace eg_03_csharp_auth_code_grant_core.Examples
+namespace eSignature.Examples
 {
     public static class CreatePermissionProfile
     {
@@ -19,24 +19,14 @@ namespace eg_03_csharp_auth_code_grant_core.Examples
         public static PermissionProfile Create(string profileName, AccountRoleSettingsExtension accountRoleSettings, string accessToken, string basePath, string accountId)
         {
             // Construct your API headers
-            var config = new Configuration(new ApiClient(basePath));
-            config.AddDefaultHeader("Authorization", "Bearer " + accessToken);
-            AccountsApi accountsApi = new AccountsApi(config);           
+            var apiClient = new ApiClient(basePath);
+            apiClient.Configuration.DefaultHeader.Add("Authorization", "Bearer " + accessToken);
+            AccountsApi accountsApi = new AccountsApi(apiClient);           
 
             var newPermissionProfile = new PermissionProfile(PermissionProfileName: profileName, Settings: accountRoleSettings);
 
             // Call the eSignature REST API
             return accountsApi.CreatePermissionProfile(accountId, newPermissionProfile);
-        }
-
-        /// <summary>
-        /// Temporary subclass for AccountRoleSettings
-        /// This class is needed for now until DCM-3905 is ready
-        /// </summary>
-        public class AccountRoleSettingsExtension : AccountRoleSettings
-        {
-            [System.Runtime.Serialization.DataMember(Name = "signingUIVersion", EmitDefaultValue = false)]
-            public string SigningUiVersion { get; set; }
         }
     }
 }
