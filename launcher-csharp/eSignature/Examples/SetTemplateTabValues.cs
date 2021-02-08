398196ce19a1acc44a325ecd8bc0f147b4e3f067
RomanBachaloSigmaSoftware <roman.bachalo@sigma.software>
2021-02-08 21:42:35 +0200

    SetTemplateTabValues updated

--- launcher-csharp/eSignature/Examples/SetTemplateTabValues.cs
+++ launcher-csharp/eSignature/Examples/SetTemplateTabValues.cs
@@ -3,7 +3,7 @@ using DocuSign.eSign.Api;
 using DocuSign.eSign.Client;
 using DocuSign.eSign.Model;
 
-namespace eg_03_csharp_auth_code_grant_core.Examples
+namespace eSignature.Examples
 {
     public static class SetTemplateTabValues
     {
@@ -26,8 +26,8 @@ namespace eg_03_csharp_auth_code_grant_core.Examples
             string accessToken, string basePath, string accountId, string templateId, string returnUrl, string pingUrl = null)
         {
             // Construct your API headers
-            var config = new Configuration(new ApiClient(basePath));
-            config.AddDefaultHeader("Authorization", "Bearer " + accessToken);
+            var apiClient = new ApiClient(basePath);
+            apiClient.Configuration.DefaultHeader.Add("Authorization", "Bearer " + accessToken);
 
             // Create Tabs and CustomFields
             // Set the values for the fields in the template
@@ -143,7 +143,7 @@ namespace eg_03_csharp_auth_code_grant_core.Examples
             };
 
             // Call the eSignature REST API
-            EnvelopesApi envelopesApi = new EnvelopesApi(config);
+            EnvelopesApi envelopesApi = new EnvelopesApi(apiClient);
             EnvelopeSummary results = envelopesApi.CreateEnvelope(accountId, envelopeAttributes);
             string envelopeId = results.EnvelopeId;
 
